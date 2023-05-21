2dd6345e6596b1c5dcf00f530837eddab383bcdf
Tranel <tranel@Tranels-MacBook-Air.local>
2023-05-21 13:47:46 -0500

    Added view database

--- questions.js
+++ questions.js
@@ -28,3 +28,47 @@ function promptMainMenu() {
     ]);
 }
 
+function viewAllDepartments() {
+    return connection
+        .promise()
+        .query(`SELECT * FROM department`)
+        .then(([rows]) => {
+            console.table(rows);
+        });
+}
+
+function viewAllRoles() {
+    return connection   
+    .promise()
+    .query(`
+    SELECT role.id, role.title, role.salary, department.name AS department
+    FROM role
+    INNER JOIN department ON role.department_id = department.id
+    `)
+    .then(([rows]) => {
+        console.table(rows);
+    });
+}
+
+function viewAllEmployees() {
+    return connection
+        .promise()
+        .query(`
+        SELECT
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            role.title AS job_title,
+            department.name AS department,
+            role.salary,
+            CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN role ON emplyee.role_id = role.id
+        LEFT JOIN department on role.department_id = department.id
+        LEFT JOIN employee manager on employee.manager_id = manager_id
+        `)
+        .then(([rows]) => {
+            console.table(rows);
+        });
+}
+
