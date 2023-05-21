6afe99d83e56ac2b3853991f63879c27c4ffdf11
Tranel <tranel@Tranels-MacBook-Air.local>
2023-05-21 18:19:02 -0500

    Added view by criteria functions

--- assets/js/view.js
+++ assets/js/view.js
@@ -44,8 +44,50 @@ function viewAllEmployees() {
         });
 }
 
+function viewEmployeesByManager() {
+    return connection
+        .promise()
+        .query(`
+        SELECT
+            manager.id AS manager_id,
+            manager.first_name AS manager_first_name,
+            manager.last_name AS manager_last_name,
+            employee.id AS employee_id,
+            employee.first_name AS employee_first_name,
+            employee.last_name AS employee_last_name,
+        FROM employee
+        INNER JOIN employee manager ON employee.manager_id = manager.id
+        ORDER BY manager.id, employee.id
+        `)
+        .then(([rows]) => {
+            console.table(rows);
+        });
+}
+
+function viewEmployeesByDepartment() {
+    return connection
+        .promise()
+        .query(`
+        SELECT
+            department.id AS department_id,
+            department.name AS department_name,
+            employee.id AS employee_id,
+            employee.first_name AS employee_first_name,
+            employee.last_name AS employee_last_name
+        FROM employee
+        INNER JOIN role ON employee.role_id = role.id
+        INNER JOIN department ON role.department_id = department.id
+        ORDER BY department.id, employee.id
+        `)
+        .then(([rows]) => {
+            console.table(rows);
+        });
+}
+
 module.exports = {
     viewAllDepartments,
     viewAllRoles,
     viewAllEmployees,
+    viewEmployeesByManager,
+    viewEmployeesByDepartment,
 }
